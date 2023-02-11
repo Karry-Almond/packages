98bcb22ff3252c0a18ec3bd5f5bb708bb13102e0
Karry-Almond <1377437271@qq.com>
2023-02-11 06:10:33 +0000

    no.8 commit

--- favoriteDB/favorite.go
+++ favoriteDB/favorite.go
@@ -1,11 +1,13 @@
 package favoriteDB
 
 import (
-	"log"
+	// "log"
 
 	"github.com/karry-almond/packages/model"
+	// "golang.org/x/tools/go/analysis/passes/nilfunc"
 )
-
+// status 返回0——成功，返回1——失败
+// err 返回nil——成功，返回其他——失败原因
 func NewFavorite(user_id int64, video_id int64) (status int32, err error) {
 
 	// 创建一条favorite数据
@@ -14,18 +16,22 @@ func NewFavorite(user_id int64, video_id int64) (status int32, err error) {
 		ID:      1,
 		UserId:  user_id,
 		VideoId: video_id}
-	result := Db.Create(&favorite)
+	if err := Db.Create(&favorite).Error; err != nil{
+		return 1, err
+	}
 
 	//更改对应video的favorite_count
 	var video model.Video
 
-	Db.Select("*").First(&model.Video{ID: video_id}).Scan(&video)
+	if err:= Db.Select("*").First(&model.Video{ID: video_id}).Scan(&video).Error; err != nil{
+		return 1, err
+	}
 
-	Db.Model(&model.Video{ID: video_id}).Update("favorite_count", video.FavoriteCount+1)
+	if err := Db.Model(&model.Video{ID: video_id}).Update("favorite_count", video.FavoriteCount+1).Error; err != nil{
+		return 1, err
+	}
 
-	log.Println(result)
-
-	return 1, nil
+	return 0, nil
 
 }
 
@@ -33,9 +39,13 @@ func CancelFavorite(user_id int64, video_id int64) (status int32, err error) {
 	//先根据user_id和video_id寻找到id，再根据id软删除
 	var favorite model.Favorite
 
-	Db.Select("*").First(&model.Favorite{UserId: user_id, VideoId: video_id}).Scan(&favorite)
+	if err := Db.Select("*").First(&model.Favorite{UserId: user_id, VideoId: video_id}).Scan(&favorite).Error; err !=nil{
+		return 1, err
+	}
 
-	Db.Delete(&favorite)
+	if err := Db.Delete(&favorite).Error; err != nil{
+		return 1, err
+	}
 
-	return 1, nil
+	return 0, nil
 }
